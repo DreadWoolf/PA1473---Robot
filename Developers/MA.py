4d387c6ea3a6456db482b2a32e1ff92ac9e1e78f
omaridris23 <omid23@student.bth.se>
2024-03-06 10:51:25 +0100

    Update MA.py

--- Developers/MA.py
+++ Developers/MA.py
@@ -1,1 +1,31 @@
-print ( "Hello World ")
+#!/usr/bin/env pybricks-micropython
+
+# Pybricks imports
+from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, TouchSensor
+from pybricks.parameters import Port, Stop, Direction, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+
+
+import random
+
+
+# Your code goes here
+# Robot definitions
+ev3 = EV3Brick()
+
+# Motor definitions
+elevationMotor = Motor(Port.B)  #8, 40
+clawMotor = Motor(Port.A)
+rotationMotor = Motor (Port.C) #12, 36
+
+
+ev3 = EV3Brick()
+clawMotor = Motor(Port.A)
+if __name__ == "__main__":
+
+    ev3.speaker.beep()
+    clawMotor.reset_angle(angle=0)
+
