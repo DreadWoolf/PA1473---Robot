8232e1a570a38e2a4ef822677ed52365c8254aa8
DreadWoolf <160495138+DreadWoolf@users.noreply.github.com>
2024-03-28 14:00:26 +0100

    uppdate arm and claw

--- Arm_and_Claw.py
+++ Arm_and_Claw.py
@@ -9,33 +9,34 @@ def Pickup(angleTarget:int, openClawsFirst:bool, height:int = 0):
     armMovement(angleTarget= angleTarget)
     clawMovement(open= (not openClawsFirst)) # If not open first will grip here.
     armMovement(angleTarget= -angleTarget)
-    # else:
-    #     clawMovement(open = openClawsFirst) # If openFirst = True will open here.
-    #     armMovement(angleTarget= angleTarget)
-
-    # if openClawsFirst == False:
-    #     clawMovement(open=(not openClawsFirst))
 
 
 def Place(angleTarget:int, openClawsFirst:bool):
     
-    # clawMovement(open = openClawsFirst) # If openFirst = True will open here.
+    # If openFirst = True will open here.
     armMovement(angleTarget= angleTarget)
     clawMovement(open= (not openClawsFirst)) # If not open first will grip here.
     armMovement(angleTarget= -angleTarget)
-    # if openClawsFirst == False:
-    #     clawMovement(open=(not openClawsFirst))
 
 
 
-def armMovement(angleTarget, operatingSpeed = 60):
+def armMovement(angleTarget: int, height:int = 0, operatingSpeed = 60):
     # Thooths on the gears of the arm.
     bigGear = 40
     smallGear = 8
     multiplyAngle = -(bigGear/smallGear)
 
     print("start arm angle " , elevationMotor.angle())
-    elevationMotor.run_angle(operatingSpeed,angleTarget * multiplyAngle)
+    ######################################
+    ######################################
+    ######################################
+    #           Här @subhi               #
+    # Bör dock vara klart för hantering om vilken höjd.
+    #           (angleTarget - height)   #
+    ######################################
+    ######################################
+    ######################################
+    elevationMotor.run_angle(operatingSpeed,(angleTarget - height) * multiplyAngle)
     print("targeted arm angle " , elevationMotor.angle())
 
 
@@ -46,7 +47,5 @@ def clawMovement(open:bool):
 
     if open:
         clawMotor.run_angle(60 ,(60) * multiplyAngle)
-    # wait(4000)
     else:
         clawMotor.run_angle(60 ,(-60) * multiplyAngle)
-    return 0
