c242c22b29220d7adfa11f44e3e4f04bb6e9b437
omaridris23 <omid23@student.bth.se>
2024-04-08 15:50:40 +0200

    Update OI.py

--- Developers/OI.py
+++ Developers/OI.py
@@ -101,7 +101,7 @@ elevationMotor.run_angle(-30,(60) * multiplyAngle)
 #print(clawMotor.run_until_stalled(40, then=Stop.BRAKE, duty_limit=None))
 # print(elevationMotor.run_until_stalled(30, then=Stop.BRAKE, duty_limit=None))'''
 #-------------^^-------------stall------------------^^--------------------------------------
-from pybricks import robotics
+'''from pybricks import robotics
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, TouchSensor
 from pybricks.parameters import Port, Stop, Direction, Color
@@ -143,7 +143,7 @@ def colorzones():
             if str(button) == "Button.CENTER":
                 counter += 1
                 ev3.screen.clear()
-                chosen = colors.pop(current_index)
+                chosen = colors.pop(current_index % len(colors))
                 print("Colors:", colors)
                 print("popped:", chosen)
                 zoneSort[chosen.lower()] = counter
@@ -154,5 +154,17 @@ def colorzones():
             print(zoneSort)   
 theend = colorzones()
 print("OKAY STOP")
-print(theend)
+print(theend)'''
 #-----------------^---------------colorzones------------^------------
+'''from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, TouchSensor
+from pybricks.parameters import Port, Stop, Direction, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+
+ev3 = EV3Brick()
+ev3.speaker.set_volume(100)
+color = "A person who thinks all the time Has nothing to think about except thoughts So... he loses touch with reality And lives in a world of illusions By thoughts I mean specifically, Chatter in the skull Perpetual and compulsive repetition of words, of reckoning and calculating I'm not saying that thinking is bad Like everybody else It's useful in moderation A good servant, but a bad master And all civilized peoples Have increasingly become crazy and self destructive Because through excessive thinking The have lost touch with reality That to say... We confuse signs With the real world... This is the beginning of meditation Most of us would have Rather money than tangible wealth And a great occasion is somehow spoiled for us unless photographed And to read about it the next day in the newspaper Is oddly more fun for us than the original event This is a disaster... For as a result of confusing the real world of nature with mere signs We are destroying nature We are so tied up in our minds that we've lost our senses Time to wake up What is reality? Obviously... no one can say Because it isn't words It isn't material, that's just an idea Reality is... The point cannot be explained in words Im not trying to put you down It's an expression of you as you are One must live... We need to survive to go on... We must go on."
+ev3.speaker.say("The color is " + color)'''
+#-------------^^------voice---^^------------
