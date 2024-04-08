a49e20856e814adbcd2f3604dec6ef8f01a63926
remopp <102233272+remopp@users.noreply.github.com>
2024-04-08 14:52:03 +0200

    yeah

--- Developers/MA.py
+++ Developers/MA.py
@@ -43,9 +43,9 @@ def manual_movment():
     temp=True
 
     while temp:
-        yeah = ev3.buttons.pressed()
-        print(yeah)
-        for button in yeah:
+        buttons = ev3.buttons.pressed()
+        print(buttons)
+        for button in buttons:
             button_str = str(button)
             if button_str == "Button.UP":
                 elevationMotor.run_angle(60,-10)
@@ -104,7 +104,7 @@ def getColor():
     return "unknown item"  # Object doesn't match any color predominantly
 
 def menu():
-    choicelist = ["start.code","zonecolor.selection", "zone.hight"]
+    choicelist = ["start_code","zonecolor_selection", "zone_hight", "set_origin"]
     current_index=0
     temp=True
     ev3.screen.print(choicelist[current_index])
@@ -125,4 +125,67 @@ def menu():
             if str(button) == "Button.CENTER":
                 ev3.screen.clear()
                 ev3.screen.print("you chose ",choicelist[current_index])
-                temp=False
+                if choicelist[current_index] == "zone_hight":
+                    zonecords = zone_hight()
+                    print(zonecords)
+                if choicelist[current_index] == "set_origin":
+                    origin = set_origin()
+                    print(origin)
+
+def zone_hight():
+    zonenum = [1,2,3]
+    zonecords = {"1":[],
+                 "2":[],
+                 "3":[]
+                 }
+    rotationMotor.reset_angle(0)
+    elevationMotor.reset_angle(0)
+    horangle = 0 
+    verangle = 0
+    temp=True
+    for num in zonenum:
+        temp=True
+        ev3.screen.clear()
+        ev3.screen.print("chose the location \n of zone: ",num)
+        while temp:
+            buttons = ev3.buttons.pressed()
+            wait(250)
+            for button in buttons:
+                button_str = str(button)
+                if button_str == "Button.UP":
+                    elevationMotor.run_angle(60,-10)
+                if button_str == "Button.DOWN":
+                    elevationMotor.run_angle(60,10)
+                elif button_str == "Button.CENTER":
+                    horangle = rotationMotor.angle() 
+                    verangle = elevationMotor.angle()
+                    zonecords[str(num)] =[horangle,verangle]
+                    if num == 3:
+                        return zonecords
+                    temp=False
+                if button_str == "Button.LEFT":
+                    rotationMotor.run_angle(60,-10)
+                if button_str == "Button.RIGHT":
+                    rotationMotor.run_angle(60,10)
+def set_origin():
+    origin=[]
+    temp=True
+    while temp:
+        buttons = ev3.buttons.pressed()
+        wait(250)
+        for button in buttons:
+            button_str = str(button)
+            if button_str == "Button.UP":
+                elevationMotor.run_angle(60,-10)
+            if button_str == "Button.DOWN":
+                elevationMotor.run_angle(60,10)
+            elif button_str == "Button.CENTER":
+                horangle = rotationMotor.angle() 
+                verangle = elevationMotor.angle()
+                origin = [horangle,verangle]
+                return origin
+            if button_str == "Button.LEFT":
+                rotationMotor.run_angle(60,-10)
+            if button_str == "Button.RIGHT":
+                rotationMotor.run_angle(60,10)
+menu()
