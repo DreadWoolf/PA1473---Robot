4998c15e784c389ec26f165b859ab57d40c91382
remopp <102233272+remopp@users.noreply.github.com>
2024-04-08 10:59:52 +0200

    yeah

--- Developers/MA.py
+++ Developers/MA.py
@@ -13,6 +13,8 @@ import random
 
 
 # Your code goes here
+colorSense = ColorSensor(Port.S2)
+pressureSense = TouchSensor(Port.S1)
 # Robot definitions
 ev3 = EV3Brick()
 
@@ -30,43 +32,76 @@ def clear_terminal():
 
 
 
+def manual_movment():
+    ###########################
+    ##### reset function ######
+    ###########################
+    rotationMotor.reset_angle(0)
+    elevationMotor.reset_angle(0)
+    horangle = 0 
+    verangle = 0
+    temp=True
 
-###########################
-##### reset function ######
-###########################
-rotationMotor.reset_angle(0)
-elevationMotor.reset_angle(0)
-horangle = 0 
-verangle = 0
-temp=True
-smallGear = 12  #Tooths for gear moving clockwise. 
-bigGear = 16   #Tooths for gear moving counter clockwise. 
-multiplyAngle = -(bigGear/smallGear)
+    while temp:
+        yeah = ev3.buttons.pressed()
+        print(yeah)
+        for button in yeah:
+            button_str = str(button)
+            if button_str == "Button.UP":
+                elevationMotor.run_angle(60,-10)
+            if button_str == "Button.DOWN":
+                elevationMotor.run_angle(60,10)
+            if button_str == "Button.CENTER":
+                horangle = rotationMotor.angle() 
+                verangle = elevationMotor.angle()
+                rotationMotor.run_angle(60,-horangle)
+                elevationMotor.run_angle(60,-verangle)
+                temp=False 
+            if button_str == "Button.LEFT":
+                rotationMotor.run_angle(60,-10)
+            if button_str == "Button.RIGHT":
+                rotationMotor.run_angle(60,10)
 
-#elevationMotor.run_angle(60,90*multiplyAngle)
+def getColor():
+    # Get RGB values from the sensor (assuming they are in the range 0-100)
+    fcolor = colorSense.color()
+    dis = 2
+    aos = 2
+    Tred, Tgreen, Tblue, Tref = 0,0,0,0
+    for i in range(aos):   
+        red, green, blue = colorSense.rgb()
+        ref = colorSense.reflection()
+        Tref += ref
+        Tred += red
+        Tgreen += green
+        Tblue += blue
+        wait(10)
+    Tred = Tred//aos
+    Tgreen = Tgreen//aos
+    Tblue = Tblue//aos
+    Tref = Tref//aos
+    Tred, Tgreen, Tblue = dis*Tred, dis*Tgreen, dis*Tblue
+    # Define margin of error
+    margin = 15  # Adjust the margin as needed
+    lmargin = 5
+    # Define colors and their conditions
+    colors = [
+        ("Red", lambda r, g, b ,re: (r > g + margin and r > g - margin) and (r > b + margin and r > b - margin ) and (r > (margin - lmargin)*dis) and  (50+lmargin>=re>=50-lmargin)),
+        ("Green", lambda r, g, b ,re: (g > r + margin and g > r - margin) and (g > b + margin and g > b - margin) and (g > (margin - lmargin)*dis) or  fcolor=="Color.GREEN"),
+        ("Blue", lambda r, g, b, re: (b > r + margin and b > r - margin) and (b > g + margin and b > g - margin) and (b > (margin - lmargin)*dis)),
+        ("Yellow", lambda r, g, b, re:(abs(g - (r/2)) <= margin) and (g > b + margin and g > b - margin) and (r > b + margin and r > b - margin ) or fcolor =="Color.YELLOW"),
+        ("Green", lambda r, g, b, re: abs(g - b) <= margin and (g > r + margin or g > r - margin) and (b > r + margin or b > r - margin) and g > (margin-lmargin)*dis and b > (margin-lmargin)*dis ),  # Condition for Greenb
+        ("nothing", lambda r, g, b, re: ((margin)>=r>=0) and ((margin)>=g>=0) and ((margin)>=b>=0))
+        # Add more colors here
+        # ("ColorName", lambda r, g, b: <condition>)
+    ]
+    
+    # Check each color condition
+    for color_name, condition in colors:
+        if condition(Tred, Tgreen, Tblue, Tref):
+            return color_name
+        
+    return "unknown item"  # Object doesn't match any color predominantly
 
-while temp:
-    yeah = ev3.buttons.pressed()
-    print(yeah)
-    for button in yeah:
-        button_str = str(button)
-        if button_str == "Button.UP":
-            elevationMotor.run_angle(60,-10)
-        if button_str == "Button.DOWN":
-            elevationMotor.run_angle(60,10)
-        if button_str == "Button.CENTER":
-            horangle = rotationMotor.angle() 
-            verangle = elevationMotor.angle()
-            rotationMotor.run_target(60,-horangle)
-            elevationMotor.run_target(60,-verangle)
-            temp=False 
-        if button_str == "Button.LEFT":
-            rotationMotor.run_angle(60,-10)
-        if button_str == "Button.RIGHT":
-            rotationMotor.run_angle(60,10)
-    print("hor angle ===",horangle)
-    print ("rael hor angle ===",rotationMotor.angle() )
-    print("ver angle ===",verangle)
-    print("real ver angle ===", elevationMotor.angle())
-
-
+while True:
+    getColor()
