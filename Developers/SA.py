03a8dbe33fd2e3078f5d301219d701fb9f2b8687
salloush0-0 <161703501+salloush0-0@users.noreply.github.com>
2024-04-25 10:49:38 +0200

    fucked_up

--- Developers/SA.py
+++ Developers/SA.py
@@ -1,163 +1,43 @@
-#!/usr/bin/env pybricks-micropython
+def armMovement(goToZone, angleTarget: int, height: int = 0, operatingSpeed = 120, calibrate: bool = False, potentialCargo = False):
+    # Thooths on the gears of the arm.
+    bigGear = 40
+    smallGear = 8
+    multiplyAngle = -(bigGear/smallGear)
+    # global Estop
+    elevationMotor.run_target(operatingSpeed, target_angle * multiplyAngle)
 
-# Should import all, and work otherwise uncomment the stuff.
-from Parameters import *
-from Arm_and_Claw import Place, Pickup, armMovement
-from rotationMotor import rotateBase
-from colorAlgorithm import colorSort
+    height = zoneHeight[goToZone]  # Gets the hegiht for the zone.
+    target_angle = angleTarget - height
 
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
+    # print("Estop  ... ", Estop)
 
-#
-def main():
-    ev3.speaker.beep()
-    #Initialize the motor controlling the claw
-    claw_motor = Motor(Port.A)
-
-# Function to get current angle of the claw
-    def get_claw_angle():
-        return claw_motor.angle()
-
-# Example usage
-    current_angle = get_claw_angle()
-    print("Current angle of the claw:", current_angle)
-
-
-    times = 2
-    zoneAmount = 3
-    potentialCargo = False
-    periodTime = 4000 # 4s (4000)
-    
-    Calibrate()
-    
-    run = 0
-    location = 1
-    lastZone = 0
-    
-    while run < times: # times = 2.
-        run += 1
-
-        goToZone = location
-
-
-        if potentialCargo:
-            sortZone = 0
-            ######################################
-            ######################################
-            ######################################
-            #       sortZone = Sort algorithm()
-            # sortZone = colorSort()
-            sortZone, color = colorSort()
-
-            if sortZone == 'Error' or sortZone == "nothing":
-
-                ## Will continue if found nothing, otherwise place the cargo.
-                if sortZone == "Error":
-                    
-                    ev3.speaker.beep()
-                    wait(4)
-                    ev3.speaker.beep()
-
-                    ### print error on robot.
-                    ev3.screen.print("ERROR, color not supported")
-                    wait(100)
-
-                    ## Drop of again, if detected random color.
-                    Place(angleTarget=-35, openClawsFirst=False)
-
-            else:
-                ### Screen print 
-                ev3.screen.print("Color: " + color + " to zone: " + str(sortZone))
-                wait(100)
-                ######################################
-                ######################################
-                ######################################
-                # Make sure this works...
-                if sortZone == 0:
-                    rotateBase(angle= zoneLocation[sortZone]) # Go to zone '0'.
-                else:
-                    # This might not work as intended... (if we want to go to the next zone for example)
-                    rotateBase(angle = zoneLocation[sortZone] - zoneLocation[lastZone])
-                # Uppdate the lastZone.
-                lastZone = sortZone  # 0
-            
-            Place(angleTarget= -35, openClawsFirst= False)
-            potentialCargo = False
-
-        else:
-            rotateBase(angle = zoneLocation[goToZone] - zoneLocation[lastZone])
-            Pickup(angleTarget= -35, openClawsFirst= True)
-
-            # picked up package true or false.
-            ######################################
-            ######################################
-            ######################################
-            # Check if we have cargo!
-            potentialCargo = True
-
-        print("Check color \nWhere to next= ", location)
-
-
-        if location >= zoneAmount:
-            location = 0
-            goToZone = 0
-        else:
-            lastZone = location
-            location += 1
-            goToZone = location
-            if location >= zoneAmount + 1:
-                location = 0
-                # LocationZero()
-                rotateBase(angle= 0)
-                wait(periodTime)  # 4000
-                ev3.speaker.beep()
-        
-        
+    if calibrate:
+        # print("start arm angle " , elevationMotor.angle())
+        elevationMotor.run_target(speed = 60, target_angle = angleTarget - height)
+        # return
+    elif height == 0 and potentialCargo and angleTarget <= 40 * multiplyAngle - 5 and angleTarget >= 40 * multiplyAngle + 5:
+        elevationMotor.run_until_stalled(operatingSpeed, then=Stop.HOLD, duty_limit=20)
+        # elevationMotor.run_stall(operatingSpeed,(angleTarget - height) * multiplyAngle)
+    else:
         ######################################
         ######################################
         ######################################
-        # Testing placment (problem since the code above is increasing before this)
-        # if location == 2 + 1:
-        #     potentialCargo = True
-        #     print("loc is True")
-                    
-
+        #           Här @subhi               #
+        # Bör dock vara klart för hantering om vilken höjd.
+        #           (angleTarget - height)   #
+        ######################################
+        ######################################
+        ######################################
         
+        elevationMotor.run_target(operatingSpeed, target_angle * multiplyAngle)
 
-        print("GoToZone = ", goToZone)
-        # goToZone = 0
+    # print("targeted arm angle " , elevationMotor.angle())
 
-        # rotateBase(angle=zoneLocation[goToZone])
-        # # zone0Calibration()
-
-        # armMovement(angleTarget= -35)
-        # print("Let go")
-        # clawMovement(open = True)
-        # armMovement(angleTarget= 35)
-        # print("Close the empty claws")
-        # clawMovement(open = False)
-        # # rotateBase(operatingSpeed= 60, angle = zoneLocation[0])
-
-    # Go back to start, if arm is higher than ground level.
-    armMovement(angleTarget= -35)
-
-
-def Calibrate():
-    # LocationZero()
-
-    print("Calibrate arm")
-    print("Calibrate claw\n\n")
-
-    rotateBase(angle= 0)
-    return 0
-
-
-
-## Checks if this is the running script, and not imported from somewhere!
-if __name__ == "__main__":
-    armMovement(angleTarget= 35)
+    # Check if the event is set
+    # if Event.is_set():
+    #     # Process the updated global variable
+    #     print("Thread 2: Global variable (Estop):", Estop)
+    #     # Clear the event
+    #     Event.clear()
+    #test(goToZone, angleTarget, calibrate)
     
-    # for i in range(3):
-    # clawMovement(False)
-    main()
